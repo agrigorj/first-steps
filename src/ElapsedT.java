d22f14e7588e8be03c16bd9e331753556eba3716
agrigorj <EATI11agnord>
2016-01-09 15:43:34 +0200

    Sudoku mang:redigeeritud ja kommenteeritud. Valmis vastamiseks

--- src/ElapsedT.java
+++ src/ElapsedT.java
@@ -1,76 +1,81 @@
-import javafx.application.Platform;
-import javafx.scene.control.Label;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
  * Created by agrigorj on 28/12/15.
+ * Klass taimeri k�ivitamiseks
  */
-public class ElapsedT extends GUI {
-   static int secondsPassed =0;
-    static Timer myTimer= new Timer();
-    static TimerTask task;
-    static int minutePassed;
-    static  int hoursPassed;
-    static String time;
+class ElapsedT extends GUI {
+    int secondsPassed;//sekundid
+    Timer myTimer = new Timer();
+    TimerTask task;
+    int minutePassed;//minutid
+    int hoursPassed;//tunnid
+    String time;//siia kirjutame aeg formaadis 00:00:00
+
+    public void startCount() {
+        //meetodi k�ivitamisel nullime tunnid, minutid, sekundid:
+        secondsPassed = 0;
+        minutePassed = 0;
+        hoursPassed = 0;
+        if (task != null)
+            return;
+        task = new TimerTask() {
+            @Override
+            public void run() {//aeg l�ks!
+                secondsPassed++; //loeme sekundid
+                if (secondsPassed == 60) {//kui on l�binud 60 sek, nullime muutujat
+                    secondsPassed = 0;
+                    minutePassed++;//kui on l�binud 60 sek, suurendame minutid 1 v�rra
+                }
+                if (minutePassed == 60) {//kui on l�binud 60 min, nullime muutujat
+                    minutePassed = 0;
+                    hoursPassed++;//kui on l�binud 60 min, suurendame tunnid 1 v�rra
+                }
+                //kirjutame aeg �les
+                String seconds = Integer.toString(secondsPassed);
+                String minutes = Integer.toString(minutePassed);
+                String hours = Integer.toString(hoursPassed);
+
+                if (secondsPassed <= 9) {
+                    //kuni 10 kirjutame 0 ette
+                    seconds = "0" + Integer.toString(secondsPassed);
+                }
+                if (minutePassed <= 9) {
+                    //kuni 10 kirjutame 0 ette
+                    minutes = "0" + Integer.toString(minutePassed);
+                }
+                if (hoursPassed <= 9) {
+                    //kuni 10 kirjutame null ettte
+                    hours = "0" + Integer.toString(hoursPassed);
+                }
 
 
-        public void startCount() {
-            secondsPassed=0;
-            if (task != null)
-                return;
-            task = new TimerTask() {
-                @Override
-                public void run() {
-                    secondsPassed++;
-                    if (secondsPassed == 60) {
-                        secondsPassed = 0;
-                        minutePassed++;
-                    }
-                    if (minutePassed == 60) {
-                        minutePassed = 0;
-                        hoursPassed++;
-                    }
-                    String seconds = Integer.toString(secondsPassed);
-                    String minutes = Integer.toString(minutePassed);
-                    String hours = Integer.toString(hoursPassed);
+                time = (hours + ":" + minutes + ":" + seconds);//aeg formaadis 00:00:00
+                getTime();//edastame aeg meetodile getTime
 
-                    if (secondsPassed <= 9) {
-                        seconds = "0" + Integer.toString(secondsPassed);
-                    }
-                    if (minutePassed <= 9) {
-                        minutes = "0" + Integer.toString(minutePassed);
-                    }
-                    if (hoursPassed <= 9) {
-                        hours = "0" + Integer.toString(hoursPassed);
-                    }
+            }
 
+        };
+        myTimer.scheduleAtFixedRate(task, 0, 1000);//timer k�ivitub kohe ja t��tab sekundite t�psusega
 
-                    time = (hours+":"+minutes+":"+seconds);
-                    getTime();
+    }
 
-                    }
-
-            };
-            myTimer.scheduleAtFixedRate(task, 0, 1000);
-
-        }
-
-    public String  getTime() {
+    public String getTime() {
+        //meetod tagastab teksti, mida kasutame GUI klassis
         String show = time;
         return show;
-
-
     }
 
     public void stopCount() {
-        if(task==null)
+        //timer seisma
+        if (task == null)
             return;
         task.cancel();
-        task=null;
+        task = null;
 
     }
 
-    }
+}
 
